6013eecee2abf10f46d5e2afc36ba62ccd6411a8
Jimancio <54536722+ignacioHourquescos@users.noreply.github.com>
2022-12-28 15:37:57 -0300

    segunda version

--- pages/home/styles.js
+++ pages/home/styles.js
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 import device from "../../utils/utils";
 
-export const Container = styled.div`
+const Container = styled.div`
 	width: 100%;
 `;
+
+export default Container;

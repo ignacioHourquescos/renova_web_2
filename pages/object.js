7d2b6e1b2c598d98d3a6f395230b8378f4dfadd1
Jimancio <54536722+ignacioHourquescos@users.noreply.github.com>
2023-02-22 14:59:03 -0300

    test funcioandno

---
+++ pages/object.js
@@ -0,0 +1,47 @@
+const object = {
+	object: "database",
+	id: "2bca360b-3aae-4517-abae-717845adbc9a",
+	cover: null,
+	icon: null,
+	created_time: "2023-02-08T19:37:00.000Z",
+	created_by: { object: "user", id: "0d79e827-b872-4e1e-aac6-ba0ec88983c9" },
+	last_edited_by: {
+		object: "user",
+		id: "0d79e827-b872-4e1e-aac6-ba0ec88983c9",
+	},
+	last_edited_time: "2023-02-09T18:30:00.000Z",
+	title: [
+		{
+			type: "text",
+			text: [Object],
+			annotations: [Object],
+			plain_text: "OFERTASS",
+			href: null,
+		},
+	],
+	description: [],
+	is_inline: false,
+	properties: {
+		Ejemplo: {
+			id: "F%5E%5CP",
+			name: "Ejemplo",
+			type: "multi_select",
+			multi_select: [Object],
+		},
+		Descripcion: {
+			id: "aMkj",
+			name: "Descripcion",
+			type: "rich_text",
+			rich_text: {},
+		},
+		Nombre: {
+			id: "title",
+			name: "Nombre",
+			type: "title",
+			title: {},
+		},
+	},
+	parent: { type: "workspace", workspace: true },
+	url: "https://www.notion.so/2bca360b3aae4517abae717845adbc9a",
+	archived: false,
+};
---
+++ pages/test.js
@@ -0,0 +1,62 @@
+const { Client } = require("@notionhq/client");
+import { useEffect } from "react";
+const notion = new Client({
+	auth: "secret_4yG0RqGh5tXyLuKm3gRMhyvRf1ygu6sNRINVQliEofc",
+});
+
+function App({ items }) {
+	useEffect(() => {
+		let array = [];
+
+		items.forEach((item) => {
+			array.push({
+				id: item.id,
+				name: item.properties.codigo.title[0].plain_text,
+				description: item.properties.titulo.rich_text[0].plain_text,
+				activo: item.properties.activo.checkbox,
+				descuento: item.properties.descuento.number,
+				image: item.properties.imagen.files[0]?.file.url,
+				//task: books[i].properties.Descripcion.id,
+			});
+		});
+
+		console.log("aca esta el array", array);
+
+		console.log("USE EFFECT" + items);
+	});
+
+	return (
+		<div className="App">
+			<p>
+				Edit <code>src/App.js</code> and save to reload.
+			</p>
+		</div>
+	);
+}
+
+export default App;
+
+//id =f81df32bac964878af140c249adf7e72
+
+export async function getStaticProps() {
+	const notion = new Client({
+		auth: "secret_4yG0RqGh5tXyLuKm3gRMhyvRf1ygu6sNRINVQliEofc",
+	});
+	const response = await notion.databases.query({
+		database_id: "2bca360b3aae4517abae717845adbc9a",
+		sorts: [
+			{
+				timestamp: "created_time",
+				direction: "ascending",
+			},
+		],
+	});
+	console.log("get static props", response);
+
+	return {
+		props: {
+			items: response.results,
+		},
+		revalidate: 1,
+	};
+}

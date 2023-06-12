46a20dd4064e7db6dc52f7582a7abdec5b9862e6
unknown <ihourquescos@gmail.com>
2023-06-12 14:13:06 -0300

    modiico imQ

--- components/ofertas/index.js
+++ components/ofertas/index.js
@@ -5,37 +5,38 @@ import Title from "./components/title/Title";
 import PromotionalCard from "./components/promotional-card/PromotionalCard";
 
 export const Ofertas = ({ categories, products }) => {
-  return (
-    <Styled.Container>
-      {categories.map(
-        (categoryElement, idx) =>
-          isBrandPresent(products, categoryElement) && (
-            <>
-              <Title key={idx}>{categoryElement}</Title>
-              {categoryElement != "DESTACADO"
-                ? products
-                    ?.filter((element) => element.category == categoryElement)
-                    .map((element, idx) => (
-                      <ProductCard key={idx} products={element} />
-                    ))
-                : products
-                    ?.filter((element) => element.category == categoryElement)
-                    .map((element, idx) => (
-                      <PromotionalCard key={idx} promotions={element} />
-                    ))}
-            </>
-          )
-      )}
-    </Styled.Container>
-  );
+	const segment = ["FRAM", "PURFLUX"];
+	return (
+		<Styled.Container>
+			{products
+				.filter((element) => element.category == "DESTACADO")
+				.map((element, idx) => (
+					<PromotionalCard key={idx} promotions={element} />
+				))}
+			{categories.map(
+				(categoryElement, idx) =>
+					isBrandPresent(products, categoryElement) && (
+						<>
+							<Title key={idx}>{categoryElement}</Title>
+
+							{products
+								?.filter((element) => element.category == categoryElement)
+								.map((element, idx) => (
+									<ProductCard key={idx} products={element} />
+								))}
+						</>
+					)
+			)}
+		</Styled.Container>
+	);
 };
 
 const isBrandPresent = (productsArray, category) => {
-  console.log("categoria", category);
-  for (let i = 0; i < productsArray.length; i++) {
-    if (productsArray[i].active && productsArray[i].category === category) {
-      return true;
-    }
-  }
-  return false;
+	console.log("categoria", category);
+	for (let i = 0; i < productsArray.length; i++) {
+		if (productsArray[i].active && productsArray[i].category === category) {
+			return true;
+		}
+	}
+	return false;
 };

d70157a0bbfe0a0f3e8ab90795fdfb21a07a84f9
unknown <ihourquescos@gmail.com>
2025-01-09 21:03:13 -0300

    fix de imagenes

--- components/ofertas/components/product-card/ProductCard.js
+++ components/ofertas/components/product-card/ProductCard.js
@@ -1,21 +1,35 @@
 import { addCommasForThousands } from "../../../../utils/brandColorFunction";
 import { Styled } from "./styles";
 import Flex, { FlexItem } from "styled-flex-component";
+import Image from "next/image";
+import { useState } from "react";
 // import { brandColorHandler } from "../../utils/brandColorFunction";
 
 const ProductCard = ({ products }) => {
+	const [imageLoaded, setImageLoaded] = useState(false);
+
 	return (
 		<>
 			{products.active && (
 				<Styled.Inner borderColor="#CF1A0E">
-					<Styled.Image imageUrl={products.imageUrl}>
-						<Styled.Brand>{products.brand}</Styled.Brand>
-						{products.type ? (
-							<Styled.Presentation>{products.type}</Styled.Presentation>
-						) : (
-							""
-						)}
-					</Styled.Image>
+					{!imageLoaded && <Styled.Placeholder />}
+					<Image
+						src={products.imageUrl}
+						alt={products.title || "Product image"}
+						width={500}
+						height={300}
+						priority={true}
+						quality={75}
+						loading="eager"
+						onLoad={() => setImageLoaded(true)}
+						style={{ opacity: imageLoaded ? 1 : 0 }}
+					/>
+					<Styled.Brand>{products.brand}</Styled.Brand>
+					{products.type ? (
+						<Styled.Presentation>{products.type}</Styled.Presentation>
+					) : (
+						""
+					)}
 					<Styled.Information>
 						<Styled.Title>{products.title}</Styled.Title>
 						<Styled.DetailContainer>
